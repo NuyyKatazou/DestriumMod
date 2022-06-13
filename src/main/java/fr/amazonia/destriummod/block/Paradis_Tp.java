3677646ffccedbc97f0c7a377a23b816a5c34360
NuyyKatazou <rotor.ytb@laposte.net>
2022-06-13 14:44:22 +0200

    ParadisTpBugPatch

--- src/main/java/fr/amazonia/destriummod/block/Paradis_Tp.java
+++ src/main/java/fr/amazonia/destriummod/block/Paradis_Tp.java
@@ -1,8 +1,10 @@
 package fr.amazonia.destriummod.block;
 
+import fr.amazonia.destriummod.init.ModBlocks;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.server.MinecraftServer;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.LogicalSidedProvider;
@@ -12,12 +14,21 @@ public class Paradis_Tp extends Block{
 
 	public Paradis_Tp(Properties p_i48440_1_) {
 		super(p_i48440_1_);
-		// TODO Auto-generated constructor stub
 	}
 	@SubscribeEvent
-	public static void PlayerRightClick(RightClickBlock event) {
-		MinecraftServer source = LogicalSidedProvider.INSTANCE.get(LogicalSide.SERVER);
-		source.getCommands().performCommand(source.createCommandSourceStack(), "execute in destriummod:paradis run teleport @p 1 100 1");
+	public static void PlayerRightClick(PlayerInteractEvent.RightClickBlock event) {
+		BlockState iblockstate = event.getWorld().getBlockState(event.getPos());
+        Block block = iblockstate.getBlock();
+        if (block == ModBlocks.PARADIS_TP.get())
+        {
+        	Paradis_Tp.onBlockActivated();
+        }
+	}
+	public static void onBlockActivated() {
+			MinecraftServer source = LogicalSidedProvider.INSTANCE.get(LogicalSide.SERVER);
+			source.getCommands().performCommand(source.createCommandSourceStack(), "gamerule sendCommandFeedback false");
+	  		source.getCommands().performCommand(source.createCommandSourceStack(), "execute in destriummod:paradis run teleport @p 1 100 1");
+	  		
 	}
 
 	}
