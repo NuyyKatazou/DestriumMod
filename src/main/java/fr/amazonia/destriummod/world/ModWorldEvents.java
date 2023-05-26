56d0163edbca7187c4991ab1e02f801b97dd250a
NuyyKatazou <rotor.ytb@laposte.net>
2023-05-26 02:31:24 +0200

    Repair Ore Generation Added

--- src/main/java/fr/amazonia/destriummod/world/ModWorldEvents.java
+++ src/main/java/fr/amazonia/destriummod/world/ModWorldEvents.java
@@ -3,7 +3,6 @@ package fr.amazonia.destriummod.world;
 import fr.amazonia.destriummod.DestriumMod;
 import fr.amazonia.destriummod.world.gen.ModFlowerGeneration;
 import fr.amazonia.destriummod.world.gen.ModOreGeneration;
-
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
