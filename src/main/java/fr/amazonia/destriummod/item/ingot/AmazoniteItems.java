1568de85a2029016e2d92f268515d48a6a7ed5ed
NuyyKatazou <rotor.ytb@laposte.net>
2023-09-15 02:28:49 +0200

    Items Descriptions Added

--- src/main/java/fr/amazonia/destriummod/item/ingot/AmazoniteItems.java
+++ src/main/java/fr/amazonia/destriummod/item/ingot/AmazoniteItems.java
@@ -1,11 +1,26 @@
 package fr.amazonia.destriummod.item.ingot;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class AmazoniteItems extends Item {
 
     public AmazoniteItems(Properties properties) {
         super(properties);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltip, TooltipFlag pFlag) {
+        pTooltip.add(Component.translatable(this.getDescriptionId() + ".desc").withStyle(ChatFormatting.BLUE));
     }
 }
 
