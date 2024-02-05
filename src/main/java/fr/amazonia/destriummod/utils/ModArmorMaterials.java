391c5ebbfcde2da29087afcf5d32f36ee087985f
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2024-02-05 16:36:51 +0100

    Space Added

--- src/main/java/fr/amazonia/destriummod/utils/ModArmorMaterials.java
+++ src/main/java/fr/amazonia/destriummod/utils/ModArmorMaterials.java
@@ -14,6 +14,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 public enum ModArmorMaterials implements IArmorMaterial {
+
 	NECKLACE("necklace", 74, new int[]{2, 5, 4, 1}, 0, SoundEvents.ARMOR_EQUIP_CHAIN, 0F, 0F, () -> {
 		   return Ingredient.of(ModItems.AMAZONITE_NUGGETS.get());
 	   }),
