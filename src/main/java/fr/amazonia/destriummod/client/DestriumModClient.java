b1e77732cb7fcb2e3bd85a7923f765c67679e03e
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2024-02-07 21:48:47 +0100

    Seeds & Below Block Render

--- src/main/java/fr/amazonia/destriummod/client/DestriumModClient.java
+++ src/main/java/fr/amazonia/destriummod/client/DestriumModClient.java
@@ -1,11 +1,24 @@
 package fr.amazonia.destriummod.client;
 
+import fr.amazonia.destriummod.init.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 public class DestriumModClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        //Plants
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.AMAZONITE_PLANTS, RenderLayer.getCutoutMipped());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.DESTRIUM_PLANTS, RenderLayer.getCutoutMipped());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.XP_PLANTS, RenderLayer.getCutoutMipped());
 
+        ///Flowers
+
+        //Blocks
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.BELOW_BLOCK, RenderLayer.getTranslucent());
+
+        //Fluids
     }
 }
