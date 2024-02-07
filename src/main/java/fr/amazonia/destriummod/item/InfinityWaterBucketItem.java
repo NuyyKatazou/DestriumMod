876dde4f03ba7f8beda0157f0fb2707c08a7270c
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2024-02-07 23:59:39 +0100

    Delete duplicate things

--- src/main/java/fr/amazonia/destriummod/item/InfinityWaterBucketItem.java
+++ src/main/java/fr/amazonia/destriummod/item/InfinityWaterBucketItem.java
@@ -39,63 +39,6 @@ public class InfinityWaterBucketItem extends BucketItem {
 	protected ItemStack getEmptySuccessItem(ItemStack p_203790_1_, PlayerEntity p_203790_2_) {
 		return !p_203790_2_.abilities.instabuild ? new ItemStack(ModItems.INFINITY_WATER_BUCKET.get()) : p_203790_1_;
 		}
-	
-	public ActionResult<ItemStack> use(World p_77659_1_, PlayerEntity p_77659_2_, Hand p_77659_3_) {
-		ItemStack itemstack = p_77659_2_.getItemInHand(p_77659_3_);
-	      RayTraceResult raytraceresult = getPlayerPOVHitResult(p_77659_1_, p_77659_2_, this.content == Fluids.EMPTY ? RayTraceContext.FluidMode.SOURCE_ONLY : RayTraceContext.FluidMode.NONE);
-	      ActionResult<ItemStack> ret = net.minecraftforge.event.ForgeEventFactory.onBucketUse(p_77659_2_, p_77659_1_, itemstack, raytraceresult);
-	      if (ret != null) return ret;
-	      if (raytraceresult.getType() == RayTraceResult.Type.MISS) {
-	         return ActionResult.pass(itemstack);
-	      } else if (raytraceresult.getType() != RayTraceResult.Type.BLOCK) {
-	         return ActionResult.pass(itemstack);
-	      } else {
-	         BlockRayTraceResult blockraytraceresult = (BlockRayTraceResult)raytraceresult;
-	         BlockPos blockpos = blockraytraceresult.getBlockPos();
-	         Direction direction = blockraytraceresult.getDirection();
-	         BlockPos blockpos1 = blockpos.relative(direction);
-	         if (p_77659_1_.mayInteract(p_77659_2_, blockpos) && p_77659_2_.mayUseItemAt(blockpos1, direction, itemstack)) {
-	            if (this.content == Fluids.EMPTY) {
-	               BlockState blockstate1 = p_77659_1_.getBlockState(blockpos);
-	               if (blockstate1.getBlock() instanceof IBucketPickupHandler) {
-	                  Fluid fluid = ((IBucketPickupHandler)blockstate1.getBlock()).takeLiquid(p_77659_1_, blockpos, blockstate1);
-	                  if (fluid != Fluids.EMPTY) {
-	                     p_77659_2_.awardStat(Stats.ITEM_USED.get(this));
-
-	                     SoundEvent soundevent = this.content.getAttributes().getFillSound();
-	                     if (soundevent == null) soundevent = fluid.is(FluidTags.LAVA) ? SoundEvents.BUCKET_FILL_LAVA : SoundEvents.BUCKET_FILL;
-	                     p_77659_2_.playSound(soundevent, 1.0F, 1.0F);
-	                     ItemStack itemstack1 = DrinkHelper.createFilledResult(itemstack, p_77659_2_, new ItemStack(fluid.getBucket()));
-	                     if (!p_77659_1_.isClientSide) {
-	                        CriteriaTriggers.FILLED_BUCKET.trigger((ServerPlayerEntity)p_77659_2_, new ItemStack(fluid.getBucket()));
-	                     }
-
-	                     return ActionResult.sidedSuccess(itemstack1, p_77659_1_.isClientSide());
-	                  }
-	               }
-
-	               return ActionResult.fail(itemstack);
-	            } else {
-	               BlockState blockstate = p_77659_1_.getBlockState(blockpos);
-	               BlockPos blockpos2 = canBlockContainFluid(p_77659_1_, blockpos, blockstate) ? blockpos : blockpos1;
-	               if (this.emptyBucket(p_77659_2_, p_77659_1_, blockpos2, blockraytraceresult)) {
-	                  this.checkExtraContent(p_77659_1_, itemstack, blockpos2);
-	                  if (p_77659_2_ instanceof ServerPlayerEntity) {
-	                     CriteriaTriggers.PLACED_BLOCK.trigger((ServerPlayerEntity)p_77659_2_, blockpos2, itemstack);
-	                  }
-
-	                  p_77659_2_.awardStat(Stats.ITEM_USED.get(this));
-	                  return ActionResult.sidedSuccess(this.getEmptySuccessItem(itemstack, p_77659_2_), p_77659_1_.isClientSide());
-	               } else {
-	                  return ActionResult.fail(itemstack);
-	               }
-	            }
-	         } else {
-	            return ActionResult.fail(itemstack);
-	         }
-	      }
-	      
-	   }
 
 	private final java.util.function.Supplier<? extends Fluid> fluidSupplier;
 	public Fluid getFluid() { return fluidSupplier.get(); }
