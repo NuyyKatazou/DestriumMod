e012d6d84be1103cf4dd2e0e97d65c67c877d482
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2024-02-11 15:04:14 +0100

    Give Overworld Portal Blocks when enter Paradis

--- src/main/java/fr/amazonia/destriummod/utils/ParadisTeleporter.java
+++ src/main/java/fr/amazonia/destriummod/utils/ParadisTeleporter.java
@@ -9,6 +9,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.common.util.ITeleporter;
@@ -45,8 +46,9 @@ public class ParadisTeleporter implements ITeleporter {
         }
         ServerPlayer player = (ServerPlayer) entity;
         if (destWorld.dimension().equals(DestriumMod.PARADIS_DIMENSION)) {
-  
+
         }
+        player.addItem(new ItemStack(ModBlocks.OVERWORLD_PORTAL_BLOCK.get()));
         player.teleportTo(destinationPos.getX() + 0.5D, destinationPos.getY() + 1D, destinationPos.getZ() + 0.5D);
         if (thisIsToParadisDim) {
             boolean doSetBlock = true;
