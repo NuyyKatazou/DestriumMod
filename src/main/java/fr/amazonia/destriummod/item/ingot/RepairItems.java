fc6e7d957a64fa5e18b7e6bc919f13676d9429ee
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2024-02-14 16:28:01 +0100

    Add Description to Repair Fragment & Ingot

---
+++ src/main/java/fr/amazonia/destriummod/item/ingot/RepairItems.java
@@ -0,0 +1,24 @@
+package fr.amazonia.destriummod.item.ingot;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class RepairItems extends Item {
+
+    public RepairItems(Item.Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack p_41421_, @Nullable Level p_41422_, List<Component> p_41423_, TooltipFlag p_41424_) {
+        p_41423_.add(new TranslatableComponent(this.getDescriptionId() + ".desc").withStyle(ChatFormatting.LIGHT_PURPLE));
+    }
+}
