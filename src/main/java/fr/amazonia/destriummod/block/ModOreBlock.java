77750e9bed8b39078ef99ccb5f876880fc57cc9e
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2024-02-14 14:04:06 +0100

    Delete useless things

--- src/main/java/fr/amazonia/destriummod/block/ModOreBlock.java
+++ src/main/java/fr/amazonia/destriummod/block/ModOreBlock.java
@@ -2,10 +2,7 @@ package fr.amazonia.destriummod.block;
 
 import fr.amazonia.destriummod.init.ModBlocks;
 import net.minecraft.block.*;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
-import net.minecraft.world.server.ServerWorld;
 
 import java.util.Random;
 
@@ -22,14 +19,5 @@ public class ModOreBlock extends OreBlock {
         } else {
             return this == ModBlocks.AMAZONITE_RELIQUE_BLOCK.get() ? MathHelper.nextInt(p_220281_1_, 5, 15) : 0;
         }
-    }
-
-    public void spawnAfterBreak(BlockState p_220062_1_, ServerWorld p_220062_2_, BlockPos p_220062_3_, ItemStack p_220062_4_) {
-        super.spawnAfterBreak(p_220062_1_, p_220062_2_, p_220062_3_, p_220062_4_);
-    }
-
-    @Override
-    public int getExpDrop(BlockState state, net.minecraft.world.IWorldReader reader, BlockPos pos, int fortune, int silktouch) {
-        return silktouch == 0 ? this.xpOnDrop(RANDOM) : 0;
     }
 }
