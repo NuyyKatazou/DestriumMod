5eb89cb1401577b73d7ca7fb8eb9bedb343cdb1b
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2024-02-15 21:17:31 +0100

    Repair Ore Spawn Range Change

--- src/main/java/fr/amazonia/destriummod/world/feature/ModPlacedFeatures.java
+++ src/main/java/fr/amazonia/destriummod/world/feature/ModPlacedFeatures.java
@@ -10,5 +10,5 @@ public class ModPlacedFeatures {
     public static final Holder<PlacedFeature> CLOVER_PLACED = PlacementUtils.register("clover_placed", ModConfiguredFeatures.CLOVER_CONFIG, RarityFilter.onAverageOnceEvery(32), InSquarePlacement.spread(), PlacementUtils.HEIGHTMAP, BiomeFilter.biome());
     public static final Holder<PlacedFeature> CLOVER_TWO_PLACED = PlacementUtils.register("clover_two_placed", ModConfiguredFeatures.CLOVER_TWO_CONFIG, RarityFilter.onAverageOnceEvery(128), InSquarePlacement.spread(), PlacementUtils.HEIGHTMAP, BiomeFilter.biome());
 
-    public static final Holder<PlacedFeature> REPAIR_ORE_PLACED = PlacementUtils.register("repair_ore_placed", ModConfiguredFeatures.REPAIR_ORE, ModOrePlacement.commonOrePlacement(1, HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(-80), VerticalAnchor.aboveBottom(256))));
+    public static final Holder<PlacedFeature> REPAIR_ORE_PLACED = PlacementUtils.register("repair_ore_placed", ModConfiguredFeatures.REPAIR_ORE, ModOrePlacement.commonOrePlacement(1, HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(-0), VerticalAnchor.aboveBottom(100))));
 }
