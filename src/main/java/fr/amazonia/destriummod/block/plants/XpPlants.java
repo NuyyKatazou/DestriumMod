0897c764f782770b453f0c084ea5ac6b4e17088e
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2024-02-27 00:50:24 +0100

    Delete Useless Things

--- src/main/java/fr/amazonia/destriummod/block/plants/XpPlants.java
+++ src/main/java/fr/amazonia/destriummod/block/plants/XpPlants.java
@@ -4,19 +4,13 @@ import java.util.Random;
 
 import fr.amazonia.destriummod.init.ModItems;
 import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CropsBlock;
 import net.minecraft.util.IItemProvider;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.shapes.ISelectionContext;
-import net.minecraft.util.math.shapes.VoxelShape;
-import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 
 public class XpPlants extends CropsBlock {
-
-	private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{Block.box(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 3.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 4.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 5.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 6.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 7.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 9.0D, 16.0D)};
 
 	public XpPlants(AbstractBlock.Properties properties) {
         super(properties);
@@ -25,10 +19,6 @@ public class XpPlants extends CropsBlock {
     protected IItemProvider getBaseSeedId() {
         return ModItems.XP_SEEDS.get();
      }
-
-    public VoxelShape getShape(BlockState p_220053_1_, IBlockReader p_220053_2_, BlockPos p_220053_3_, ISelectionContext p_220053_4_) {
-        return SHAPE_BY_AGE[p_220053_1_.getValue(this.getAgeProperty())];
-    }
 
     public boolean isBonemealSuccess(World p_180670_1_, Random p_180670_2_, BlockPos p_180670_3_, BlockState p_180670_4_) {
         return false;
