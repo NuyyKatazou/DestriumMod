45d8e351f591b39d4e997af8e6b0ac23dd01715b
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2024-03-21 15:13:06 +0100

    Add Description to Repair Fragment & Ingot

--- src/main/java/fr/amazonia/destriummod/init/ModItems.java
+++ src/main/java/fr/amazonia/destriummod/init/ModItems.java
@@ -85,8 +85,8 @@ public class ModItems {
 	public static final RegistryObject<Item> DESTRIUM_INGOT = ITEMS.register("destrium_ingot", () -> new DestriumIngot(new Item.Properties().rarity(ModRarity.DESTRIUM).fireResistant()));
 	public static final RegistryObject<Item> ARGONIDE_NUGGETS = ITEMS.register("argonide_nuggets", () -> new ArgonideNuggets(new Item.Properties().rarity(ModRarity.ARGONIDE).fireResistant()));
 	public static final RegistryObject<Item> ARGONIDE_INGOT = ITEMS.register("argonide_ingot", () -> new ArgonideIngot(new Item.Properties().rarity(ModRarity.ARGONIDE).fireResistant()));
-	public static final RegistryObject<Item> REPAIR_FRAGMENT = ITEMS.register("repair_fragment", () -> new Item(new Item.Properties().rarity(ModRarity.REPAIR).fireResistant()));
-	public static final RegistryObject<Item> REPAIR_INGOT = ITEMS.register("repair_ingot", () -> new Item(new Item.Properties().rarity(ModRarity.REPAIR).fireResistant()));
+	public static final RegistryObject<Item> REPAIR_FRAGMENT = ITEMS.register("repair_fragment", () -> new RepairItems(new Item.Properties().rarity(ModRarity.REPAIR).fireResistant()));
+	public static final RegistryObject<Item> REPAIR_INGOT = ITEMS.register("repair_ingot", () -> new RepairItems(new Item.Properties().rarity(ModRarity.REPAIR).fireResistant()));
 
 	public static final RegistryObject<Item> EMERALD_NUGGETS = ITEMS.register("emerald_nuggets", () -> new EmeraldNuggets(new Item.Properties().rarity(ModRarity.EMERALD)));
 	public static final RegistryObject<Item> DIAMOND_NUGGETS = ITEMS.register("diamond_nuggets", () -> new DiamondNuggets(new Item.Properties().rarity(ModRarity.DIAMOND)));
