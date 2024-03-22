58ce65c0c10e26e962577de9dc7a738705df3d6e
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2024-03-22 18:25:17 +0100

    add space

--- src/main/java/fr/amazonia/destriummod/block/OverworldPortalBlocks.java
+++ src/main/java/fr/amazonia/destriummod/block/OverworldPortalBlocks.java
@@ -2,7 +2,7 @@ package fr.amazonia.destriummod.block;
 
 import net.minecraft.world.level.block.Block;
 
-public class OverworldPortalBlocks extends Block{
+public class OverworldPortalBlocks extends Block {
 
 	public OverworldPortalBlocks(Properties p_i48440_1_) {
 		super(p_i48440_1_);
