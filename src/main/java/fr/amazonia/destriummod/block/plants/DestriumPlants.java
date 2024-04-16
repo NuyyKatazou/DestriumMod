13777de0880fe490c463504dd7c18a5badae02bf
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2024-04-17 01:32:15 +0200

    Delete useless things

--- src/main/java/fr/amazonia/destriummod/block/plants/DestriumPlants.java
+++ src/main/java/fr/amazonia/destriummod/block/plants/DestriumPlants.java
@@ -4,19 +4,13 @@ import java.util.Random;
 
 import fr.amazonia.destriummod.init.ModItems;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class DestriumPlants extends CropBlock {
-
-	private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{Block.box(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 3.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 4.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 5.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 6.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 7.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 9.0D, 16.0D)};
 
 	public DestriumPlants(BlockBehaviour.Properties properties) {
 	      super(properties);
@@ -26,9 +20,6 @@ public class DestriumPlants extends CropBlock {
 	      return ModItems.DESTRIUM_SEEDS.get();
 	   }
 
-	public VoxelShape getShape(BlockState p_51330_, BlockGetter p_51331_, BlockPos p_51332_, CollisionContext p_51333_) {
-		return SHAPE_BY_AGE[p_51330_.getValue(this.getAgeProperty())];
-	}
 
 	public boolean isBonemealSuccess(Level p_180670_1_, Random p_180670_2_, BlockPos p_180670_3_, BlockState p_180670_4_) {
 		return false;
