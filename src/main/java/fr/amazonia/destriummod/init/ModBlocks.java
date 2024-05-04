bfeee4f16b990122aa9787d36f8ff9a964103963
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2024-05-04 15:38:55 +0200

    Name change in section name

--- src/main/java/fr/amazonia/destriummod/init/ModBlocks.java
+++ src/main/java/fr/amazonia/destriummod/init/ModBlocks.java
@@ -24,7 +24,8 @@ import java.util.function.Supplier;
 public class ModBlocks {
 
 	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, DestriumMod.MODID);
-	//Blocks (Paradis)
+
+	//Paradis
 	public static final RegistryObject<Block> CLOUD = registerBlock("cloud", () -> new Block(BlockBehaviour.Properties.of(Material.CLAY).strength(2.5f, 25f).requiresCorrectToolForDrops()));
 	public static final RegistryObject<Block> BLUE_CLOUD = registerBlock("blue_cloud", () -> new Block(BlockBehaviour.Properties.of(Material.CLAY).strength(3f, 25f).requiresCorrectToolForDrops()));
 	public static final RegistryObject<Block> BLACK_CLOUD = registerBlock("black_cloud", () -> new Block(BlockBehaviour.Properties.of(Material.CLAY).strength(3f, 25f).requiresCorrectToolForDrops()));
@@ -51,7 +52,7 @@ public class ModBlocks {
 	public static final RegistryObject<Block> CRACKED_CLOUD = registerBlock("cracked_cloud", () -> new Block(BlockBehaviour.Properties.of(Material.CLAY).strength(2.5f, 25f).requiresCorrectToolForDrops()));
 	public static final RegistryObject<Block> CLOUD2 = registerBlock("cloud2", () -> new Block(BlockBehaviour.Properties.of(Material.CLAY).strength(2.5f, 25f).requiresCorrectToolForDrops()));
 
-	//(Ore)
+	//Ore
 	public static final RegistryObject<Block> RANDOM_ORE = registerBlock("random_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(5f, 100f).requiresCorrectToolForDrops(), UniformInt.of(1, 5)));
 	public static final RegistryObject<Block> PARADIS_AMAZONITE_ORE = registerBlock("paradis_amazonite_ore", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(10f, 100f).requiresCorrectToolForDrops()));
 	public static final RegistryObject<Block> PARADIS_DESTRIUM_ORE = registerBlock("paradis_destrium_ore", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(6f, 50f).requiresCorrectToolForDrops()));
@@ -66,7 +67,7 @@ public class ModBlocks {
 	public static final RegistryObject<Block> ARGONIDE_BLOCK = registerBlock("argonide_block", () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(10f, 25f).requiresCorrectToolForDrops()));
 	public static final RegistryObject<Block> REPAIR_BLOCK = registerBlock("repair_block", () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(8f, 32f).requiresCorrectToolForDrops()));
 
-	//(Compressed)
+	//Compressed
 	public static final RegistryObject<Block> COBBLESTONE_COMPRESSED1 = registerBlock("cobblestone_compressed1", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(3.0F, 6.5F).requiresCorrectToolForDrops()));
 	public static final RegistryObject<Block> COBBLESTONE_COMPRESSED2 = registerBlock("cobblestone_compressed2", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(5.0F, 7.0F).requiresCorrectToolForDrops()));
 	public static final RegistryObject<Block> COBBLESTONE_COMPRESSED3 = registerBlock("cobblestone_compressed3", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(8.0F, 9.0F).requiresCorrectToolForDrops()));
@@ -82,12 +83,13 @@ public class ModBlocks {
 	public static final RegistryObject<Block> DIRT_COMPRESSED4 = registerBlock("dirt_compressed4", () -> new Block(BlockBehaviour.Properties.of(Material.DIRT).strength(12.0F, 10.0F).requiresCorrectToolForDrops()));
 	public static final RegistryObject<Block> DIRT_COMPRESSED5 = registerBlock("dirt_compressed5", () -> new Block(BlockBehaviour.Properties.of(Material.DIRT).strength(20.0F, 14.0F).requiresCorrectToolForDrops()));
 
-	//(Other)
+	//Other
 	public static final RegistryObject<Block> AMAZONITE_RELIQUE_BLOCK = registerBlock("amazonite_relique_block", () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.METAL).strength(25.0F, 100.0F).requiresCorrectToolForDrops(), UniformInt.of(5, 15)));
 	
 	
 	public static final RegistryObject<Block> BELOW_BLOCK = registerBlock("below_block", () -> new Block(BlockBehaviour.Properties.of(Material.GLASS).strength(4.0F, 10.0F).requiresCorrectToolForDrops()));
-	
+
+	//Portal Blocks
 	public static final RegistryObject<Block> PARADIS_PORTAL_BLOCK = registerBlock("paradis_portal_block", () -> new ParadisPortalBlocks(BlockBehaviour.Properties.of(Material.METAL).sound(SoundType.AMETHYST).strength(10.0F, 200.0F).requiresCorrectToolForDrops()));
 	public static final RegistryObject<Block> OVERWORLD_PORTAL_BLOCK = registerBlock("overworld_portal_block", () -> new OverworldPortalBlocks(BlockBehaviour.Properties.of(Material.METAL).sound(SoundType.AMETHYST).strength(10.0F, 200.0F).requiresCorrectToolForDrops()));
 
--- src/main/java/fr/amazonia/destriummod/init/ModItems.java
+++ src/main/java/fr/amazonia/destriummod/init/ModItems.java
@@ -75,7 +75,7 @@ public class ModItems {
 	public static final RegistryObject<Item> LUCKY_NECKLACE = ITEMS.register("lucky_necklace", () -> new LuckyNecklaceArmor(ModArmorMaterials.NECKLACE, ArmorItem.Type.CHESTPLATE, new Item.Properties().setNoRepair().durability(-1).rarity(ModRarity.DESTRIUM)));
 
 
-	//Items (Mineral)
+	//Mineral
 	public static final RegistryObject<Item> AMAZONITE_CRUMB = ITEMS.register("amazonite_crumb", () -> new Item(new Item.Properties().rarity(ModRarity.AMAZONITE).fireResistant()));
 	public static final RegistryObject<Item> AMAZONITE_DUST = ITEMS.register("amazonite_dust", () -> new Item(new Item.Properties().rarity(ModRarity.AMAZONITE).fireResistant()));
 	public static final RegistryObject<Item> AMAZONITE_FRAGMENT = ITEMS.register("amazonite_fragment", () -> new Item(new Item.Properties().rarity(ModRarity.AMAZONITE).fireResistant()));
@@ -110,10 +110,12 @@ public class ModItems {
 	public static final RegistryObject<Item> DESTRIUM_PLATE = ITEMS.register("destrium_plate", () -> new Item(new Item.Properties().rarity(ModRarity.DESTRIUM)));
 	public static final RegistryObject<Item> AMAZONITE_PLATE = ITEMS.register("amazonite_plate", () -> new Item(new Item.Properties().rarity(ModRarity.AMAZONITE)));
 
-	//(Fluid)
+
+	//Fluids
 	public static final RegistryObject<Item> POISON_STAR = ITEMS.register("poison_star", () -> new Item(new Item.Properties().rarity(ModRarity.RED)));
 	
 	public static final RegistryObject<Item> RESISTANCE_STAR = ITEMS.register("resistance_star", () -> new Item(new Item.Properties().rarity(ModRarity.YELLOW)));
+
 
 	//Seeds
 	public static final RegistryObject<Item> DESTRIUM_SEEDS = ITEMS.register("destrium_seeds", () -> new ItemNameBlockItem(ModBlocks.DESTRIUM_PLANTS.get(), (new Item.Properties().rarity(ModRarity.DESTRIUM).fireResistant())));
