ce6980e6e5d8b3d847b2f0cc9bbd73c84095a62d
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2025-12-02 12:48:35 +0100

    Update Tags class

--- src/main/java/fr/amazonia/destriummod/utils/ModTags.java
+++ src/main/java/fr/amazonia/destriummod/utils/ModTags.java
@@ -3,11 +3,25 @@ package fr.amazonia.destriummod.utils;
 import fr.amazonia.destriummod.DestriumMod;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
 public class ModTags {
+
     public static class Blocks {
+
+        public static final TagKey<Block> AMAZONITE_ORE = tag("amazonite_ore");
+        public static final TagKey<Block> DESTRIUM_ORE = tag("destrium_ore");
+        public static final TagKey<Block> ARGONIDE_ORE = tag("argonide_ore");
+
+        public static final TagKey<Block> CLOUDS = tag("clouds");
+
+        public static final TagKey<Block> COMPRESSED_COBBLESTONE = tag("compressed_cobblestone");
+        public static final TagKey<Block> COMPRESSED_DIRT = tag("compressed_dirt");
+
+        public static final TagKey<Block> CROPS = tag("crops");
 
         public static final TagKey<Block> NEEDS_ARGONIDE_TOOL
                 = tag("needs_argonide_tool");
@@ -25,6 +39,17 @@ public class ModTags {
 
         private static TagKey<Block> forgeTag(String name) {
             return BlockTags.create(new ResourceLocation("forge", name));
+        }
+    }
+
+    public static class Items {
+
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(new ResourceLocation(DestriumMod.MODID, name));
+        }
+
+        private static TagKey<Item> forgeTag(String name) {
+            return ItemTags.create(new ResourceLocation("forge", name));
         }
     }
 }
