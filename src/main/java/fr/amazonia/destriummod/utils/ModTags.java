9a09f56727a7eb062f6061643e351a91861be8f4
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2026-02-04 23:55:19 +0100

    Add Tags

--- src/main/java/fr/amazonia/destriummod/utils/ModTags.java
+++ src/main/java/fr/amazonia/destriummod/utils/ModTags.java
@@ -34,6 +34,20 @@ public class ModTags {
 
     public static class Items {
 
+        public static final TagKey<Item> PLATES = createTag("plates");
+
+        public static final TagKey<Item> AMAZONITE_ARMORS = createTag("amazonite_armors");
+        public static final TagKey<Item> DESTRIUM_ARMORS = createTag("destrium_armors");
+        public static final TagKey<Item> ARGONIDE_ARMORS = createTag("argonide_armors");
+
+        public static final TagKey<Item> BOOTS = createTag("boots");
+
+        public static final TagKey<Item> NECKLACE = createTag("necklace");
+
+        public static final TagKey<Item> AMAZONITE_TOOLS = createTag("amazonite_tools");
+        public static final TagKey<Item> DESTRIUM_TOOLS = createTag("destrium_tools");
+        public static final TagKey<Item> ARGONIDE_TOOLS = createTag("argonide_tools");
+
         private static TagKey<Item> createTag(String name) {
             return ItemTags.create(new ResourceLocation(DestriumMod.MODID, name));
         }
