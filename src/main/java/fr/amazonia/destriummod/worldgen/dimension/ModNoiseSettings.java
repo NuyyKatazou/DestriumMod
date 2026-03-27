d581948d3640423c53fdec7fccbdddb1b9fba291
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2026-03-27 23:03:56 +0100

    Update NoiseSettings

--- src/main/java/fr/amazonia/destriummod/worldgen/dimension/ModNoiseSettings.java
+++ src/main/java/fr/amazonia/destriummod/worldgen/dimension/ModNoiseSettings.java
@@ -11,26 +11,16 @@ import net.minecraft.world.level.levelgen.SurfaceRules;
 
 public class ModNoiseSettings {
 
-    public static final ResourceKey<NoiseGeneratorSettings> PARADIS_NOISE_SETTINGS = ResourceKey.create(
-            Registries.NOISE_SETTINGS,
-            ResourceLocation.fromNamespaceAndPath(DestriumMod.MODID, "paradis_noise_settings"));
+    public static final ResourceKey<NoiseGeneratorSettings> PARADIS_NOISE_SETTINGS = ResourceKey.create(Registries.NOISE_SETTINGS, ResourceLocation.fromNamespaceAndPath(DestriumMod.MODID, "paradis_noise_settings"));
 
     public static void bootstrapNoiseSettings(BootstrapContext<NoiseGeneratorSettings> context) {
         NoiseGeneratorSettings paradisSettings = NoiseGeneratorSettings.floatingIslands(context);
         SurfaceRules.RuleSource paradisSurface = SurfaceRules.sequence(
-
-                SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR,
-                        SurfaceRules.state(ModBlocks.CLOUD.get().defaultBlockState())),
-                SurfaceRules.ifTrue(SurfaceRules.UNDER_FLOOR,
-                        SurfaceRules.state(ModBlocks.CLOUD.get().defaultBlockState())),
-                SurfaceRules.ifTrue(SurfaceRules.ON_CEILING,
-                        SurfaceRules.state(ModBlocks.CLOUD.get().defaultBlockState())),
-                SurfaceRules.ifTrue(SurfaceRules.UNDER_CEILING,
-                        SurfaceRules.state(ModBlocks.CLOUD.get().defaultBlockState())),
-                SurfaceRules.ifTrue(SurfaceRules.DEEP_UNDER_FLOOR,
-                        SurfaceRules.state(ModBlocks.CRACKED_CLOUD.get().defaultBlockState()))
-
-        );
+                SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR, SurfaceRules.state(ModBlocks.CLOUD.get().defaultBlockState())),
+                SurfaceRules.ifTrue(SurfaceRules.UNDER_FLOOR, SurfaceRules.state(ModBlocks.CLOUD.get().defaultBlockState())),
+                SurfaceRules.ifTrue(SurfaceRules.ON_CEILING, SurfaceRules.state(ModBlocks.CLOUD.get().defaultBlockState())),
+                SurfaceRules.ifTrue(SurfaceRules.UNDER_CEILING, SurfaceRules.state(ModBlocks.CLOUD.get().defaultBlockState())),
+                SurfaceRules.ifTrue(SurfaceRules.DEEP_UNDER_FLOOR, SurfaceRules.state(ModBlocks.CRACKED_CLOUD.get().defaultBlockState())));
 
         context.register(PARADIS_NOISE_SETTINGS, new NoiseGeneratorSettings(
                 paradisSettings.noiseSettings(),
