5b47bdb515ea996f387747766ab0546925c458f9
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2026-03-29 20:09:58 +0200

    Add back Override

--- src/main/java/fr/amazonia/destriummod/block/OverworldPortalBlocks.java
+++ src/main/java/fr/amazonia/destriummod/block/OverworldPortalBlocks.java
@@ -19,6 +19,7 @@ public class OverworldPortalBlocks extends Block {
         super(pProperties);
     }
 
+    @Override
     public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
         if (pPlayer.getVehicle() != null || pPlayer.isVehicle()) {
             return InteractionResult.FAIL;
