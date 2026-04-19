7f97d288357aa7c9aa5a427e3491791dedfd6cbb
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2026-04-19 19:06:03 +0200

    Add fireResistant to necklace

--- src/main/java/fr/amazonia/destriummod/init/ModItems.java
+++ src/main/java/fr/amazonia/destriummod/init/ModItems.java
@@ -76,8 +76,8 @@ public class ModItems {
     public static final RegistryObject<Item> ADVENTURER_BOOTS = ITEMS.register("adventurer_boots", () -> new AdventurerArmorBoots(ModArmorMaterials.DESTRIUM, ArmorItem.Type.BOOTS, new Item.Properties().rarity(ModRarity.DESTRIUM).fireResistant()));
     public static final RegistryObject<Item> REINFORCED_ADVENTURER_BOOTS = ITEMS.register("reinforced_adventurer_boots", () -> new ReinforcedAdventurerArmorBoots(ModArmorMaterials.AMAZONITE, ArmorItem.Type.BOOTS, new Item.Properties().rarity(ModRarity.AMAZONITE).fireResistant()));
 
-    public static final RegistryObject<Item> NECKLACE = ITEMS.register("necklace", () -> new NecklaceArmor(ModArmorMaterials.NECKLACE, ArmorItem.Type.CHESTPLATE, new Item.Properties().setNoRepair().durability(-1).rarity(ModRarity.BASIC)));
-    public static final RegistryObject<Item> LUCKY_NECKLACE = ITEMS.register("lucky_necklace", () -> new LuckyNecklaceArmor(ModArmorMaterials.NECKLACE, ArmorItem.Type.CHESTPLATE, new Item.Properties().setNoRepair().durability(-1).rarity(ModRarity.DESTRIUM)));
+    public static final RegistryObject<Item> NECKLACE = ITEMS.register("necklace", () -> new NecklaceArmor(ModArmorMaterials.NECKLACE, ArmorItem.Type.CHESTPLATE, new Item.Properties().setNoRepair().durability(-1).rarity(ModRarity.BASIC).fireResistant()));
+    public static final RegistryObject<Item> LUCKY_NECKLACE = ITEMS.register("lucky_necklace", () -> new LuckyNecklaceArmor(ModArmorMaterials.NECKLACE, ArmorItem.Type.CHESTPLATE, new Item.Properties().setNoRepair().durability(-1).rarity(ModRarity.DESTRIUM).fireResistant()));
 
 
     //Minerals
