eed632cb8ed56d4693e8025e5ba63fbaac92a6cd
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2026-04-21 22:42:28 +0200

    Update Fluids

--- src/main/java/fr/amazonia/destriummod/fluid/ResistanceLavaFluidBlock.java
+++ src/main/java/fr/amazonia/destriummod/fluid/ResistanceLavaFluidBlock.java
@@ -35,12 +35,12 @@ public class ResistanceLavaFluidBlock extends LiquidBlock {
 
     @Override
     public void neighborChanged(BlockState pState, Level pLevel, BlockPos pPos, Block pBlock, BlockPos pFromPos, boolean pIsMoving) {
-        if (this.shouldSpreadLiquid(pLevel, pPos)) {
+        if (this.shouldSpreadLiquid(pLevel, pPos, pState)) {
             pLevel.scheduleTick(pPos, pState.getFluidState().getType(), this.getFluid().getTickDelay(pLevel));
         }
     }
 
-    private boolean shouldSpreadLiquid(Level pLevel, BlockPos pPos) {
+    private boolean shouldSpreadLiquid(Level pLevel, BlockPos pPos, BlockState pState) {
         if (this.getFluid().is(FluidTags.LAVA)) {
             boolean flag = pLevel.getBlockState(pPos.below()).is(Blocks.SOUL_SOIL);
 
@@ -48,13 +48,13 @@ public class ResistanceLavaFluidBlock extends LiquidBlock {
                 BlockPos blockpos = pPos.relative(direction.getOpposite());
                 if (pLevel.getFluidState(blockpos).is(FluidTags.WATER)) {
                     Block block = pLevel.getFluidState(pPos).isSource() ? Blocks.OBSIDIAN : ModBlocks.COBBLESTONE_COMPRESSED1.get();
-                    pLevel.setBlockAndUpdate(pPos, net.minecraftforge.event.ForgeEventFactory.fireFluidPlaceBlockEvent(pLevel, pPos, pPos, block.defaultBlockState()));
+                    pLevel.setBlockAndUpdate(pPos, block.defaultBlockState());
                     this.fizz(pLevel, pPos);
                     return false;
                 }
 
                 if (flag && pLevel.getBlockState(blockpos).is(Blocks.BLUE_ICE)) {
-                    pLevel.setBlockAndUpdate(pPos, net.minecraftforge.event.ForgeEventFactory.fireFluidPlaceBlockEvent(pLevel, pPos, pPos, Blocks.BASALT.defaultBlockState()));
+                    pLevel.setBlockAndUpdate(pPos, Blocks.BASALT.defaultBlockState());
                     this.fizz(pLevel, pPos);
                     return false;
                 }
