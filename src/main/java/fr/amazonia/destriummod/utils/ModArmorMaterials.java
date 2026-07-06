96a8c2a06906197bf304c02c91bab2d8b64aaadb
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2026-07-06 17:00:02 +0200

    Update ArmorMaterial

--- src/main/java/fr/amazonia/destriummod/utils/ModArmorMaterials.java
+++ src/main/java/fr/amazonia/destriummod/utils/ModArmorMaterials.java
@@ -19,33 +19,33 @@ import java.util.function.Supplier;
 
 public class ModArmorMaterials {
 
-    public static final Holder<ArmorMaterial> NECKLACE = register("necklace", Util.make(new EnumMap<>(ArmorItem.Type.class), p_327098_ -> {
-        p_327098_.put(ArmorItem.Type.BOOTS, 3);
-        p_327098_.put(ArmorItem.Type.LEGGINGS, 6);
-        p_327098_.put(ArmorItem.Type.CHESTPLATE, 5);
-        p_327098_.put(ArmorItem.Type.HELMET, 1);
-        p_327098_.put(ArmorItem.Type.BODY, 5);
+    public static final Holder<ArmorMaterial> NECKLACE = register("necklace", Util.make(new EnumMap<>(ArmorItem.Type.class), attribute -> {
+        attribute.put(ArmorItem.Type.BOOTS, 3);
+        attribute.put(ArmorItem.Type.LEGGINGS, 6);
+        attribute.put(ArmorItem.Type.CHESTPLATE, 5);
+        attribute.put(ArmorItem.Type.HELMET, 1);
+        attribute.put(ArmorItem.Type.BODY, 5);
     }), 0, SoundEvents.ARMOR_EQUIP_CHAIN, 0.0F, 0.0F, () -> Ingredient.of(ModItems.AMAZONITE_NUGGETS.get()));
-    public static final Holder<ArmorMaterial> ARGONIDE = register("argonide", Util.make(new EnumMap<>(ArmorItem.Type.class), p_327096_ -> {
-        p_327096_.put(ArmorItem.Type.BOOTS, 4);
-        p_327096_.put(ArmorItem.Type.LEGGINGS, 7);
-        p_327096_.put(ArmorItem.Type.CHESTPLATE, 9);
-        p_327096_.put(ArmorItem.Type.HELMET, 4);
-        p_327096_.put(ArmorItem.Type.BODY, 12);
+    public static final Holder<ArmorMaterial> ARGONIDE = register("argonide", Util.make(new EnumMap<>(ArmorItem.Type.class), attribute -> {
+        attribute.put(ArmorItem.Type.BOOTS, 4);
+        attribute.put(ArmorItem.Type.LEGGINGS, 7);
+        attribute.put(ArmorItem.Type.CHESTPLATE, 9);
+        attribute.put(ArmorItem.Type.HELMET, 4);
+        attribute.put(ArmorItem.Type.BODY, 12);
     }), 18, SoundEvents.ARMOR_EQUIP_DIAMOND, 3.5F, 0.4F, () -> Ingredient.of(ModItems.ARGONIDE_INGOT.get()));
-    public static final Holder<ArmorMaterial> DESTRIUM = register("destrium", Util.make(new EnumMap<>(ArmorItem.Type.class), p_327102_ -> {
-        p_327102_.put(ArmorItem.Type.BOOTS, 5);
-        p_327102_.put(ArmorItem.Type.LEGGINGS, 8);
-        p_327102_.put(ArmorItem.Type.CHESTPLATE, 10);
-        p_327102_.put(ArmorItem.Type.HELMET, 5);
-        p_327102_.put(ArmorItem.Type.BODY, 14);
+    public static final Holder<ArmorMaterial> DESTRIUM = register("destrium", Util.make(new EnumMap<>(ArmorItem.Type.class), attribute -> {
+        attribute.put(ArmorItem.Type.BOOTS, 5);
+        attribute.put(ArmorItem.Type.LEGGINGS, 8);
+        attribute.put(ArmorItem.Type.CHESTPLATE, 10);
+        attribute.put(ArmorItem.Type.HELMET, 5);
+        attribute.put(ArmorItem.Type.BODY, 14);
     }), 3, SoundEvents.ARMOR_EQUIP_DIAMOND, 4.0F, 0.1F, () -> Ingredient.of(ModItems.DESTRIUM_INGOT.get()));
-    public static final Holder<ArmorMaterial> AMAZONITE = register("amazonite", Util.make(new EnumMap<>(ArmorItem.Type.class), p_327103_ -> {
-        p_327103_.put(ArmorItem.Type.BOOTS, 9);
-        p_327103_.put(ArmorItem.Type.LEGGINGS, 12);
-        p_327103_.put(ArmorItem.Type.CHESTPLATE, 14);
-        p_327103_.put(ArmorItem.Type.HELMET, 9);
-        p_327103_.put(ArmorItem.Type.BODY, 22);
+    public static final Holder<ArmorMaterial> AMAZONITE = register("amazonite", Util.make(new EnumMap<>(ArmorItem.Type.class), attribute -> {
+        attribute.put(ArmorItem.Type.BOOTS, 9);
+        attribute.put(ArmorItem.Type.LEGGINGS, 12);
+        attribute.put(ArmorItem.Type.CHESTPLATE, 14);
+        attribute.put(ArmorItem.Type.HELMET, 9);
+        attribute.put(ArmorItem.Type.BODY, 22);
     }), 25, SoundEvents.ARMOR_EQUIP_NETHERITE, 5.0F, 0.1F, () -> Ingredient.of(ModItems.AMAZONITE_FRAGMENT.get()));
 
     private static Holder<ArmorMaterial> register(
