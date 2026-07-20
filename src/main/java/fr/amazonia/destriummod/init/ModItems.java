1c4953a9be3627e15b35a0d8a8e3e89c467c6133
NuyyKatazou <87091331+NuyyKatazou@users.noreply.github.com>
2026-07-20 14:07:02 +0200

    Fix necklace instant break

--- src/main/java/fr/amazonia/destriummod/init/ModItems.java
+++ src/main/java/fr/amazonia/destriummod/init/ModItems.java
@@ -19,6 +19,7 @@ import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.world.item.*;
+import net.minecraft.world.item.component.Unbreakable;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -77,8 +78,8 @@ public class ModItems {
     public static final RegistryObject<Item> ADVENTURER_BOOTS = ITEMS.register("adventurer_boots", () -> new AdventurerArmorBoots(ModArmorMaterials.DESTRIUM, ArmorItem.Type.BOOTS, component(new Item.Properties().durability(ArmorItem.Type.BOOTS.getDurability(478)).fireResistant(), "adventurer_boots", ChatFormatting.DARK_GREEN)));
     public static final RegistryObject<Item> REINFORCED_ADVENTURER_BOOTS = ITEMS.register("reinforced_adventurer_boots", () -> new ReinforcedAdventurerArmorBoots(ModArmorMaterials.AMAZONITE, ArmorItem.Type.BOOTS, component(new Item.Properties().durability(ArmorItem.Type.BOOTS.getDurability(1256)).fireResistant(), "reinforced_adventurer_boots", ChatFormatting.DARK_AQUA)));
 
-    public static final RegistryObject<Item> NECKLACE = ITEMS.register("necklace", () -> new NecklaceArmor(ModArmorMaterials.NECKLACE, ArmorItem.Type.CHESTPLATE, component(new Item.Properties().durability(-1).fireResistant(), "necklace", ChatFormatting.GRAY)));
-    public static final RegistryObject<Item> LUCKY_NECKLACE = ITEMS.register("lucky_necklace", () -> new LuckyNecklaceArmor(ModArmorMaterials.NECKLACE, ArmorItem.Type.CHESTPLATE, component(new Item.Properties().durability(-1).fireResistant(), "lucky_necklace", ChatFormatting.DARK_GREEN)));
+    public static final RegistryObject<Item> NECKLACE = ITEMS.register("necklace", () -> new NecklaceArmor(ModArmorMaterials.NECKLACE, ArmorItem.Type.CHESTPLATE, component(new Item.Properties().fireResistant().stacksTo(1).component(DataComponents.UNBREAKABLE, new Unbreakable(true)), "necklace", ChatFormatting.GRAY)));
+    public static final RegistryObject<Item> LUCKY_NECKLACE = ITEMS.register("lucky_necklace", () -> new LuckyNecklaceArmor(ModArmorMaterials.NECKLACE, ArmorItem.Type.CHESTPLATE, component(new Item.Properties().stacksTo(1).fireResistant().component(DataComponents.UNBREAKABLE, new Unbreakable(true)), "lucky_necklace", ChatFormatting.DARK_GREEN)));
 
 
     //Minerals
